470c8d2a2b701814b6e5d222db47e03df9612fec
Özgür Adem Işıklı <i.ozguradem@gmail.com>
2015-01-30 10:28:25 +0200

    Added custom message

--- src/Ozziest/Maintenance/Maintenance.php
+++ src/Ozziest/Maintenance/Maintenance.php
@@ -2,16 +2,17 @@
 
 class Maintenance {
 
-	/**
-	 * Application maintenance mode is checking
-	 *
-	 * @return null
-	 */
-	public static function check($path)
-	{
-		if (file_exists(getcwd().'/'.$path.'/down')) {
-			die('Be right back!');
-		}
-	}
+    /**
+     * Application maintenance mode is checking
+     *
+     * @param  string       $message        [optional]
+     * @return null
+     */
+    public static function check($path, $message = 'Be right back!')
+    {
+        if (file_exists(getcwd().'/'.$path.'/down')) {
+            die($message);
+        }
+    }
 
 }
